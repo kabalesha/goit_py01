7573fde22432e41f1eb946981c9d617c8f262ae6
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-12-02 19:55:33 +0100

    fix bugs

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import random
 from datetime import datetime
-from enum import unique
-
 
 # Task 1
+
 def get_days_from_today(user_input):
-    user_date = datetime.strptime(user_input, '%Y-%m-%d')
+    try:
+        user_date = datetime.strptime(user_input, '%Y-%m-%d')
+    except ValueError:
+        return None
     current_date = datetime.now()
     delta_days = (user_date - current_date).days
-    if delta_days > 0:
-        print(f"{delta_days} days left")
-    else:
-        user_date = user_date.replace(year=user_date.year)
-        delta_days = (user_date - current_date).days
-        print(f"{delta_days} days left")
-
-get_days_from_today("2023-08-21")
+    if delta_days == 0:
+        return 0
+    return delta_days
+days_left = get_days_from_today("2023-08-21")
 
 # Task 2
 
-def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 100 or quantity < 1 or quantity > (max - min + 1):
-        return print("Write relevant numbers")
-    unique_nums = random.sample(range(min, max + 1), quantity)
-    return unique_nums
-
+def get_numbers_ticket(min_val, max_val, quantity):
+    if min_val < 1 or max_val > 100 or quantity < 1 or quantity > (max_val - min_val + 1):
+        return []
+    unique_nums = random.sample(range(min_val, max_val + 1), quantity)
+    return sorted(unique_nums)
 lottery_numbers = get_numbers_ticket(1, 100, 10)
-print("Ваші лотерейні числа:", lottery_numbers)
